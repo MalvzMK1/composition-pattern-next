902b4e6e33337cefeb5de4f8d6ece2d281b729e8
Matheus Alves <matheus.adeoliveira11@gmail.com>
2023-07-12 21:14:28 -0300

    Index file to export the component

---
+++ src/components/Notification/index.tsx
@@ -0,0 +1,11 @@
+import { NotificationIcon } from './NotificationIcon';
+import { NotificationActions } from './NotificationActions';
+import { NotificationContent } from './NotificationContent';
+import { NotificationRoot } from './NotificationRoot';
+
+export const Notification = {
+	Root: NotificationRoot,
+	Icon: NotificationIcon,
+	Actions: NotificationActions,
+	Content: NotificationContent,
+};
